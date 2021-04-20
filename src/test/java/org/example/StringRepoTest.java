b2a746122e773730f0282bfb24104e16071631ab
Erik Svensson <eosvensson@gmail.com>
2021-04-20 11:42:40 +0200

    Added some comments

--- src/test/java/org/example/StringRepoTest.java
+++ src/test/java/org/example/StringRepoTest.java
@@ -10,21 +10,25 @@ public class StringRepoTest{
 
     @Before
     public void setUp() throws Exception {
-
+        StringRepo.clear(); //First empty the array
+        StringRepo.setStrings(new String[]{"Erik", "Olof", "Jörgen"}); //Then adding some data
     }
 
     @Test
     public void boo() {
+        //Before each test we have a predictable environment
         System.out.println(Arrays.toString(StringRepo.getStrings()));
     }
 
     @Test
     public void foo() {
+        //Before each test we have a predictable environment
         System.out.println(Arrays.toString(StringRepo.getStrings()));
     }
 
     @Test
     public void name() {
+        //Before each test we have a predictable environment
         System.out.println(Arrays.toString(StringRepo.getStrings()));
     }
 }
